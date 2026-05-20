6e785d9db5b099b6435a84e8e616010bcf570bd9
mnacik1988 <mnacik1988@gmail.com>
2026-05-20 15:17:37 +0300

    Update Firebase config with credentials and enable
    Updated Firebase configuration with actual credentials and enabled options.

--- firebase-config.js
+++ firebase-config.js
@@ -4,16 +4,16 @@
 // 4. Change enabled to true.
 
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyAumaD8KnKABjYrfCRB_MDIHKWGen4vEoM",
+  authDomain: "movie-mood-6ea4c.firebaseapp.com",
+  projectId: "movie-mood-6ea4c",
+  storageBucket: "movie-mood-6ea4c.firebasestorage.app",
+  messagingSenderId: "73836944599",
+  appId: "1:73836944599:web:bac525f037786ec8236492",
 };
 
 export const firebaseOptions = {
-  enabled: false,
+  enabled: true,
   moviesCollection: "movies",
   storageFolder: "movie-posters"
 };
